9c2cb53536ccb371b4c51d0867006c5be8da634e
Dhanush <dhanushkrish54@gmail.com>
2023-12-20 12:11:22 +0530

    Update advanced-objects.js | 12:11PM

--- advanced-objects.js
+++ advanced-objects.js
@@ -126,4 +126,45 @@ functionality.beep();      // SINCE functionality = robot.functionality; ===> ro
 
 
 // ***************** Destructured Assignment ******************* //
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+const {functionality} = robot;
+//const functionality = robot.functionality;
+functionality.beep();
+
+
+// ***************** Built-in Object Methods ******************* //
+const robot = {
+  model: "SAL-1000",
+  mobile: true,
+  sentient: false,
+  armor: "Steel-plated",
+  energyLevel: 75,
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot);
+
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({ laserBlaster: true, voiceRecognition: true }, robot);
+
+console.log(newRobot);
+
 
