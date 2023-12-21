e3feb64de8229d498014fee5f36a532f85bbc0a1
Dhanush <dhanushkrish54@gmail.com>
2023-12-21 14:50:57 +0530

    Update basics.js | 2:50PM

--- basics.js
+++ basics.js
@@ -114,3 +114,79 @@ const myStorage = {
 // To assign the value 'maps' in the object 'myStorage' 👇
 const gloveBoxContents = myStorage.car.inside["glove box"];
 
+
+// **************** Accessing Nested Array of objects ****************** //
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+
+// **************** OBJECTIVE - Record Collection ****************** //
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(value === ""){
+    delete records[id][prop];
+  }
+  else if(prop !== "tracks" && value !== ""){
+    records[id][prop] = value;
+  } else if(prop === "tracks" && value !== "") {
+    if(records[id].hasOwnProperty("tracks") === false){
+      records[id][prop] = [];
+    }
+    records[id][prop].push(value);
+  }
+  return records;
+}
+// function updateRecords(records, id, prop, value) {
+//   if (value === "") {
+//     delete records[id][prop];
+//   } else if (prop !== "tracks" && value !== "") {
+//     records[id][prop] = value;
+//   } else if (prop === "tracks" && value !== "") {
+//     if (records[id].hasOwnProperty("tracks") === false) {
+//       records[id][prop] = [];
+//     }
+//     records[id][prop].push(value);
+//   }
+//   return records;
+// }
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+
